6ee51b2dff1006d2271afdc4bd1492c253089189
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-11-27 19:39:30 +0200

    Update bsh.js

--- bsh.js
+++ bsh.js
@@ -1,4 +1,8 @@
+$(document).ready(function(){
+$(".addBookForm button:nth(0)")on("click", function(){
 var newBook = {};
-newBook.bookName= $(".addBookForm input:nth(0)").val()
-newBook.authorName= $(".addBookForm input:nth(1)").val()
-newBook.score= $(".addBookForm input:nth(2)").val()
+newBook.bookName= $(".addBookForm input:nth(0)").val();
+newBook.authorName= $(".addBookForm input:nth(1)").val();
+newBook.score= $(".addBookForm input:nth(2)").val();
+};
+};
