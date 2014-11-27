8486dfa5a32cac106a3168eaa1ca98ef6780e9bf
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-11-27 20:36:06 +0200

    Update bsh.js

--- bsh.js
+++ bsh.js
@@ -1,9 +1,21 @@
-var newBook = {};
+var books = [];
+
 $(document).ready(function(){
-  $(".addBookForm button:nth(0)")on("click", function(){
-  newBook.bookName= $(".addBookForm input:nth(0)").val();
-  newBook.authorName= $(".addBookForm input:nth(1)").val();
-  newBook.score= $(".addBookForm input:nth(2)").val();
-  console.log(newBook);
+  $(".addBookForm button").on("click", function(){
+    var newBook = {};
+    newBook.bookName = $(".addBookForm input:nth(0)").val();
+    newBook.authorName = $(".addBookForm input:nth(1)").val();
+    newBook.score = $(".addBookForm input:nth(2)").val();
+    
+    var newLi = $("<li>");
+    var nameDiv = $("<div>");
+    nameDiv.html(newBook.bookName);
+    var authorDiv = $("<div>");
+    authorDiv.html(newBook.authorName);
+    var scoreDiv = $("<div>");
+    scoreDiv.html(newBook.score);
+    newLi.append(nameDiv).append(authorDiv).append(scoreDiv);
+    $("ul").append(newLi);
+    books.push(newBook);
   });
 });
